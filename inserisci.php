b82d97ba558a50af8d1166a39805497a1c219c36
EmanueleSantoruvo <santoruvo.emanuele.04122005@iissvoltadegemmis.edu.it>
2024-04-18 09:44:12 +0200

    Update inserisci.php

--- inserisci.php
+++ inserisci.php
@@ -9,13 +9,12 @@ $check_query="SELECT * FROM Persone WHERE Nome='$nome' AND Cognome='$cognome' AN
 $check_res=$db_connection->query($check_query);
 
 if ($check_res->num_rows>0){
-    echo "Impossibile inserire i dati,la riga esiste già nel database.";
+    die();
 }
 else{
     $sql="INSERT INTO Persone (Nome, Cognome, DataNascita) VALUES ('$nome', '$cognome', '$datanascita')";
     $query=$db_connection->query($sql);
-    echo "Dati inseriti con successo."; 
 }
 
 $db_connection->close();
-?>
+?>
