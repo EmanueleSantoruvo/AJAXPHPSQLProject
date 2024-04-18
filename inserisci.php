2dcb5e5ce8b08e9ec797d66b02fc6107023980d1
EmanueleSantoruvo <santoruvo.emanuele.04122005@iissvoltadegemmis.edu.it>
2024-04-18 10:14:33 +0200

    Update inserisci.php

--- inserisci.php
+++ inserisci.php
@@ -5,16 +5,9 @@ $nome=$_POST['nome'];
 $cognome=$_POST['cognome'];
 $datanascita=$_POST['datanascita'];
 
-$check_query="SELECT * FROM Persone WHERE Nome='$nome' AND Cognome='$cognome' AND DataNascita='$datanascita'";
-$check_res=$db_connection->query($check_query);
+$sql="INSERT INTO Persone (Nome, Cognome, DataNascita) VALUES ('$nome', '$cognome', '$datanascita')";
+$query=$db_connection->query($sql);
 
-if ($check_res->num_rows>0){
-    die();
-}
-else{
-    $sql="INSERT INTO Persone (Nome, Cognome, DataNascita) VALUES ('$nome', '$cognome', '$datanascita')";
-    $query=$db_connection->query($sql);
-}
 
 $db_connection->close();
-?>
+?>
